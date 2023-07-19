1ddb15a1ed4250119c721be71e568d5b5e8bd56b
NguyenQuanBao <114391082+NguyenQuanBao@users.noreply.github.com>
2023-07-19 08:52:27 +0700

    Add files via upload

---
+++ CountdownImplement/src/CountdownRunnable.java
@@ -0,0 +1,13 @@
+public class CountdownRunnable implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 10 ; i >=1 ; i--){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
